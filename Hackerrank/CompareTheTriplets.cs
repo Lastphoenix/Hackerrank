246bdf332a95bb5f7693af7f4e9b8ed2c0e5c97c
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2017-03-16 13:28:42 -0300

    AC
    AC third!!

--- Hackerrank/CompareTheTriplets.cs
+++ Hackerrank/CompareTheTriplets.cs
@@ -6,5 +6,27 @@ using System.Threading.Tasks;
 
 class CompareTheTriplets
 {
+    static void Main(String[] args)
+    {
+        String[] aliceTriplet = Console.ReadLine().Split(' ');
+        int[] aliceArray = Array.ConvertAll(aliceTriplet, Int32.Parse);
+        String[] bobTriplet = Console.ReadLine().Split(' ');
+        int[] bobArray = Array.ConvertAll(bobTriplet, Int32.Parse);
+        int alice = 0;
+        int bob = 0;
+
+        for(int i = 0;i < 3; i++)
+        {
+            if (aliceArray[i] > bobArray[i])
+            {
+                alice++;
+            }
+            if(aliceArray[i] < bobArray[i])
+            {
+                bob++;
+            }
+        }
+        Console.WriteLine(alice + " " + bob);
+    }
 }
 
