e64af0519783012c0fad0dcf4d1919c1d757825a
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2017-03-16 11:35:47 -0300

    Second program
    Second program

---
+++ Hackerrank/SimpleArraySum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SimpleArraySum {
+
+    static void Main(String[] args)
+    {
+
+        int numberOfElements = Convert.ToInt32(Console.ReadLine());
+        String[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        int add = 0;
+
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            add = add + arr[i];
+        }
+        Console.WriteLine(add);
+    }
+}
+
