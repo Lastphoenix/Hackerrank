9880bdfa3ef944f6836d6137e3504ad181942881
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2017-03-17 09:41:34 -0300

    trying
    trying

--- Hackerrank/AVeryBigSum.cs
+++ Hackerrank/AVeryBigSum.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hackerrank
-{
-    class AVeryBigSum
+class AVeryBigSum
     {
+    static void Main(String[] args)
+    { 
+        long n = Int64.Parse(Console.ReadLine());
+        string[] bigArray = Console.ReadLine().Split(' ');
+        long[] arr = Array.ConvertAll(bigArray, Int64.Parse);
+        long add = 0;
 
+        for(int i = 0; i < n; i++){
+            add = add + arr[i];
+            }
+        Console.WriteLine(add);
     }
 }
---
+++ Hackerrank/DiagonalDifference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank
+{
+    class DiagonalDifference
+    {
+        static void Main(String[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            int[][] matrix = new int[n][n];
+            
+            for(int i = 0;i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+        }
+    }
+}
